f55842c86ff52bc8584a233779f6b9c715c60760
satlawa <satlawa@protonmail.com>
2022-10-29 17:22:38 +0200

    configure test file and add test case

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,7 +15,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = f"postgresql://{'postgres'}:{'ocb'}@{'localhost:5432'}/{self.database_name}"
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -33,6 +33,18 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+
+    def test_get_categories_happy_path(self):
+        """test if we get the right respose fron the /category endpoint"""
+        
+        response = self.client().get("/categories")
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+        self.assertEqual(len(data["categories"]), len(Category.query.all()))
+
 
 
 # Make the tests conveniently executable
