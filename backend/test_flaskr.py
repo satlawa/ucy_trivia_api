46c3d186f4bdd6e032066ea2456cf691c45af0c4
satlawa <satlawa@protonmail.com>
2022-10-30 11:57:27 +0100

    add test cases for search, category, play

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -1,7 +1,9 @@
 import os
+from unicodedata import category
 import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
+from jmespath import search
 
 from flaskr import create_app
 from models import setup_db, Question, Category
@@ -60,8 +62,8 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], True)
 
         # check body
-        self.assertTrue(data['total_questions'])
-        self.assertTrue(len(data['questions'])<=data['total_questions'])
+        self.assertTrue(data['totalQuestions'])
+        self.assertTrue(len(data['questions'])<=data['totalQuestions'])
         self.assertTrue(len(data['questions'])<=10)
         self.assertEqual(len(data["questions"]), len(Category.query.all()))
 
@@ -90,14 +92,14 @@ class TriviaTestCase(unittest.TestCase):
         self.question_id = data['created']
 
 
-    def test_create_question_missing_question(self):
+    def test_create_question_invalid_question_body(self):
         """test if we get the right respose when creating an 
         invalid question at endpoint /questions"""
 
         question = {
             'answer': 'instructions per second',
             'difficulty': 3,
-            'category': 5
+            'category': 1
         }
         
         response = self.client().post("/questions", json=question)
@@ -140,6 +142,120 @@ class TriviaTestCase(unittest.TestCase):
         # check body
         self.assertEqual(data['message'], 'Not Found')
 
+######
+    
+    def test_search_question_happy_path(self):
+        """test if we get the right respose when serching for a search term 
+        at endpoint /questions/search"""
+
+        search_term = "movie"
+
+        response = self.client().post(f"/questions/search", json=search_term)
+        data = json.loads(response.data)
+
+        # check status code
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+        # check body
+        self.assertTrue(data['totalQuestions'])
+        self.assertTrue(len(data['questions'])<=data['totalQuestions'])
+        self.assertTrue(len(data['questions'])<=10)
+        #self.assertEqual(len(data["questions"]), min(len(Question.query.filter(Question.question.ilike(f"%{search_term}%")).all()), 10))
+
+
+    def test_search_question_invalid_search_term(self):
+        """test if we get the right respose when serching without a search term 
+        at endpoint /questions/search"""
+
+        search_term = ""
+
+        response = self.client().post(f"/questions/search", json=search_term)
+        data = json.loads(response.data)
+
+        # check status code
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], False)
+
+        # check body
+        self.assertEqual(data['message'], 'Not Found')
+
+######
+    
+    def test_get_questions_category_happy_path(self):
+        """test if we get the right respose when asking for questions for a 
+        specific category at endpoint /categories/<int:category_id>/questions"""
+
+        category_id = 3
+
+        response = self.client().get(f"/categories/{category_id}/questions")
+        data = json.loads(response.data)
+
+        # check status code
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+        # check body
+        self.assertTrue(data['totalQuestions'])
+        self.assertTrue(len(data['questions'])<=data['totalQuestions'])
+        self.assertTrue(len(data['questions'])<=10)
+        self.assertEqual(data['currentCategory'], category_id)
+
+
+    def test_get_questions_category_for_invalid_category(self):
+        """test if we get the right respose when asking for questions for a 
+        invalid category at endpoint /categories/<int:category_id>/questions"""
+
+        category_id = 0
+
+        response = self.client().get(f"/categories/{category_id}/questions")
+        data = json.loads(response.data)
+
+        # check status code
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], True)
+
+        # check body
+        self.assertEqual(data['message'], 'Not Found')
+
+######
+    
+    def test_play_quiz_happy_path(self):
+        """test if we get the right respose when asking for questions for a 
+        specific category at endpoint /categories/<int:category_id>/questions"""
+
+        request = {
+            {'previous_questions': [10, 11],
+            'quiz_category': {'type': 'Science', 'id': 1}}}
+
+        response = self.client().post(f"/quizzes", json=request)
+        data = json.loads(response.data)
+
+        # check status code
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+        # check body
+        self.assertEqual(data['question'], 1)
+
+
+    def test_play_quiz_invalid_request(self):
+        """test if we get the right respose when asking for questions for a 
+        specific category at endpoint /categories/<int:category_id>/questions"""
+
+        request = {
+            {'previous_questions': [10, 11]}
+        }
+
+        response = self.client().post(f"/quizzes", json=request)
+        data = json.loads(response.data)
+
+        # check status code
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data['success'], False)
+
+        # check body
+        self.assertEqual(data['message'], 'Bad Request')
 
 
 # Make the tests conveniently executable
